a4ddd1d94bbd0a4bcfbeaf07b933cea2ccaaa336
vikas.sachdeva <techie.vikas.sachdeva@gmail.com>
2019-01-09 13:55:45 +0900

    project structure created

---
+++ spring.jsample/boot/rest-json/src/main/java/spring/jsample/App.java
@@ -0,0 +1,13 @@
+package spring.jsample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
