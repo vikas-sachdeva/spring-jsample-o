9e826e58b5b99469e51daa868da3daa7c5a8c963
vikas.sachdeva <techie.vikas.sachdeva@gmail.com>
2019-01-09 16:50:20 +0900

    directory structure updated

---
+++ boot/rest-json/src/main/java/spring/jsample/service/AppService.java
@@ -0,0 +1,34 @@
+package spring.jsample.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import spring.jsample.dao.AppDao;
+import spring.jsample.model.Application;
+
+@Service
+public class AppService {
+
+	@Autowired
+	private AppDao dao;
+
+	public List<Application> getApps() {
+		return dao.getApps();
+	}
+
+	public Application addApp(Application app) {
+		int id = dao.addApp(app);
+		return dao.getAppById(id);
+	}
+
+	public void deleteApp(int id) {
+		dao.deleteApp(id);
+	}
+
+	public Application updateApp(Application app) {
+		dao.updateApp(app);
+		return dao.getAppById(app.getId());
+	}
+}
