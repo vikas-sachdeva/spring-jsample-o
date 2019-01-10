e121f908b243277b71057d3858b728829ff5e089
vikas.sachdeva <techie.vikas.sachdeva@gmail.com>
2019-01-10 20:29:48 +0900

    aspect method entry exit logging

---
+++ boot/aspect-method-entry-exit-logging/src/main/java/spring/jsample/App.java
@@ -0,0 +1,15 @@
+package spring.jsample;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(App.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
+
+	}
+}
---
+++ boot/aspect-method-entry-exit-logging/src/main/java/spring/jsample/aop/AopLogConf.java
@@ -0,0 +1,88 @@
+package spring.jsample.aop;
+
+import java.util.Arrays;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.context.annotation.Configuration;
+
+@Aspect
+@Configuration
+public class AopLogConf {
+
+	private static final String ADVICE_EXP = "execution(* spring.jsample..*.*(..))";
+
+	/**
+	 * @Before annotation means execute aspect before execution of matched method
+	 * 
+	 */
+	@Before(ADVICE_EXP)
+	public void before(JoinPoint joinPoint) {
+
+		System.out.println("@Before " + joinPoint);
+	}
+
+	/**
+	 * @After annotation means execute aspect when the method executed successfully
+	 *        or it threw an exception.
+	 * 
+	 */
+	@After(ADVICE_EXP)
+	public void after(JoinPoint joinPoint) {
+		System.out.println("@After " + joinPoint);
+	}
+
+	/**
+	 * @AfterReturning annotation means execute aspect when the method executed
+	 *                 successfully
+	 */
+	@AfterReturning(ADVICE_EXP)
+	public void afterReturning(JoinPoint joinPoint) {
+		System.out.println("@AfterReturning " + joinPoint);
+	}
+
+	/**
+	 * @AfterThrowing annotation means execute aspect when the method threw an
+	 *                exception.
+	 */
+	@AfterThrowing(ADVICE_EXP)
+	public void afterThrowing(JoinPoint joinPoint) {
+		System.out.println("@AfterThrowing " + joinPoint);
+	}
+
+	/**
+	 * @throws Throwable
+	 * @AfterThrowing annotation means execute aspect when the method threw an
+	 *                exception.
+	 */
+	@Around(ADVICE_EXP)
+	public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+
+		StringBuilder entryMsg = new StringBuilder("Entering method ").append(joinPoint.getSignature());
+		entryMsg.append(" with parameters ").append(Arrays.asList(joinPoint.getArgs()));
+
+		System.out.println(entryMsg);
+
+		Object returnValue;
+		try {
+			returnValue = joinPoint.proceed();
+			StringBuilder exitMsg = new StringBuilder("Existing method ").append(joinPoint.getSignature());
+			exitMsg.append(" with return type ").append(returnValue);
+
+			System.out.println(exitMsg);
+
+		} catch (Throwable e) {
+			StringBuilder exitMsg = new StringBuilder("Existing method ").append(joinPoint.getSignature());
+			exitMsg.append(" with exception ").append(e);
+			System.out.println(exitMsg);
+			throw e;
+		}
+		return returnValue;
+	}
+}
